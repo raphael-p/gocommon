5cd3571799eb902322960889a3fa3b936a3c87c7
Raphael Piccolin <raphael.piccolin@outlook.com>
2024-09-16 00:52:37 +0200

    chore: comment + readability

--- path.go
+++ path.go
@@ -7,17 +7,16 @@ import (
 )
 
 // Checks if we are running the program as its executable.
-// if so, fetch the executable directory. Otherwise, returns an
-// empty string
+// If so, fetch the executable directory; otherwise, returns an
+// empty string.
 func GetExecDirectory(executableName string) string {
 	if os.Args[0] == executableName {
-		// if executed from compiled binary, use its directory
 		ex, err := os.Executable()
 		if err != nil {
 			panic(fmt.Sprintf("failed to locate executable: %s", err))
 		}
 		return filepath.Dir(ex)
-	} else {
-		return ""
 	}
+
+	return ""
 }
